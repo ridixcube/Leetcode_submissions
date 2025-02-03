483864e6db60632062ad4e84330bcd76a42c96e8
Riddhima Langer <147099559+ridixcube@users.noreply.github.com>
2025-02-03 12:55:19 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (64.71%) - LeetSync

---
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <algorithm> 
+
+class Solution {
+public:
+    int longestMonotonicSubarray(std::vector<int>& nums) {
+        if (nums.empty()) return 0;         
+        int result = 1; 
+        int asc = 1;    
+        int desc = 1;   
+
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] > nums[i - 1]) {
+                asc++;
+                desc = 1; 
+            } else if (nums[i] < nums[i - 1]) {
+                desc++;
+                asc = 1; 
+            } else {
+                asc = 1;
+                desc = 1;
+            }
+            result = std::max(result, std::max(asc, desc));
+        }
+        return result;        
+    }
+};

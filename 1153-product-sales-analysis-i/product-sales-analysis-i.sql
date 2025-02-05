1ecd00b41f77ac996f006dffbfd2d850d77806ba
Riddhima Langer <147099559+ridixcube@users.noreply.github.com>
2025-02-05 13:20:27 +0530

    Time: 2236 ms (45.93%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,2 +1,3 @@
-# Write your MySQL query statement below
-select p.product_name, s.year, s.price from Sales s Join Product p ON s.product_id = p.product_id;
+select p.product_name, s.year, s.price 
+FROM Sales s 
+Join Product p ON s.product_id = p.product_id;

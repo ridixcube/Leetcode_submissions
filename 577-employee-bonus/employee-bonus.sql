24b3c5d92bffe923d2121ba434c40488fd4a5829
Riddhima Langer <147099559+ridixcube@users.noreply.github.com>
2025-02-22 18:42:50 +0530

    Time: 1913 ms (36.37%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,3 +1,5 @@
-select Employee.name, Bonus.bonus from Employee
-Left Join Bonus ON Employee.empID = Bonus.empID
-Where Bonus IS null or bonus < 1000 ;
+select Employee.name, Bonus.bonus 
+from Employee
+Left Join Bonus 
+ON Employee.empID = Bonus.empID
+Where Bonus is NULL or Bonus < 1000  ;

ffd9680e5f519d92101267f628ac1c353a2484a6
Riddhima Langer <147099559+ridixcube@users.noreply.github.com>
2025-03-05 13:51:33 +0530

    Time: 2382 ms (13.38%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -4,5 +4,5 @@ FROM Students s
 CROSS JOIN Subjects sb
 LEFT JOIN Examinations e ON s.student_id = e.student_id 
 AND sb.subject_name = e.subject_name
-GROUP BY s.student_id,s.student_name,sb.subject_name
-ORDER BY s.student_id,s.student_name,sb.subject_name;
+GROUP BY s.student_id,sb.subject_name
+ORDER BY s.student_id;

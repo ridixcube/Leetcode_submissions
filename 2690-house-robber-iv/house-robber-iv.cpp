4d82617a74573b2ddbf126205f2d4d8212152e17
Riddhima Langer <147099559+ridixcube@users.noreply.github.com>
2025-03-15 23:30:37 +0530

    Time: 17 ms (87.18%) | Memory: 60.9 MB (40.42%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.cpp
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
     int minCapability(vector<int>& nums, int k) {
-        int max = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] > max) {
-                max = nums[i];
-            }
-        }
-        int left = 0;
-        int right = max;
-        int result = max;
+        int maxVal = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+            maxVal = nums[i] > maxVal ? nums[i] : maxVal;
+
+        int left = 1, right = maxVal, result = maxVal;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
             int count = 0;
-            int i = 0;
-            while (i < nums.size()) {
-                if (nums[i] <= mid) {
-                    count++;
-                    i += 2;
-                } else {
-                    i++;
-                }
-            }
-            if (count >= k) {
-                result = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
+
+            for (int i = 0; i < nums.size();) 
+                i = nums[i] <= mid ? (count++, i + 2) : i + 1;
+
+            count >= k ? (result = mid, right = mid - 1) : left = mid + 1;
         }
+
         return result;
     }
 };

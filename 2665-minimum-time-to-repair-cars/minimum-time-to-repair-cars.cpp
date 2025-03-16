5fc7a9d6193f477c27302a7f786bf68e1f598baf
Riddhima Langer <147099559+ridixcube@users.noreply.github.com>
2025-03-17 00:01:56 +0530

    Time: 18 ms (88.14%) | Memory: 59.4 MB (45.76%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -4,18 +4,24 @@ public:
         int n = ranks.size();
         int maxRank = ranks[0];
         for (int rank : ranks) {
-            if (rank > maxRank) maxRank = rank;
+            if (rank > maxRank)
+                maxRank = rank;
         }
         long long left = 0, right = (long long)maxRank * cars * cars;
-        while (left <= right) {
+
+        while (left < right) {
             long long mid = left + (right - left) / 2;
             long long count = 0;
             for (int i = 0; i < n; i++) {
                 count += sqrt(mid / ranks[i]);
-                if (count >= cars) break;
+                if (count >= cars)
+                    break;
             }
-            if (count >= cars) right = mid - 1;
-            else left = mid + 1;
+            if (count >= cars) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
         return left;
     }

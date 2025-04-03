9769a35d62282979e2bc08c7e5f38e41a062add7
Riddhima Langer <147099559+ridixcube@users.noreply.github.com>
2025-04-03 10:59:50 +0530

    Time: 33 ms (90.99%) | Memory: 13.2 MB (50.49%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n+1,INT_MAX);
+        dp[0] = 0;
+        int count = 1;
+        while(count*count <= n){
+            int square = count*count;
+            for(int i = square; i< n+1; i++){
+                dp[i] = min(dp[i - square] + 1, dp[i]);
+            }
+            count++;
+        }
+        return dp[n];        
+    }
+};

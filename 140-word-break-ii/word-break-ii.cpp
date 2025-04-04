e70e0f4ef84708da8060f72ceeaedb59c7b919ec
Riddhima Langer <147099559+ridixcube@users.noreply.github.com>
2025-04-05 01:33:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (26.74%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,22 +1,25 @@
 class Solution {
-    void backtrack(string& s, unordered_set<string>& words, string sentence, vector<string>& ans, int start) {
-        if (start == s.size()) {
-            ans.push_back(sentence.substr(1));  // Remove leading space
-            return;
-        }
-
-        for (int end = start + 1; end <= s.size(); end++) {
-            string word = s.substr(start, end - start);
-            if (words.count(word))
-                backtrack(s, words, sentence + " " + word, ans, end);
-        }
-    }
-
 public:
-    vector<string> wordBreak(string s, vector<string>& wordDict) {
+    vector<vector<string>> dp;
+    int n;
+
+    vector<string> dfs(string& s, vector<string>& wordDict, int start) {
+        if (start == n) return {""};
+        if (!dp[start].empty()) return dp[start];
         vector<string> ans;
-        unordered_set<string> words(wordDict.begin(), wordDict.end());
-        backtrack(s, words, "", ans, 0);
-        return ans;
+        for (string& w : wordDict) {
+            int sz = w.size();
+            if (start+sz <= n && s.substr(start, sz) == w) {
+                vector<string> rest = dfs(s, wordDict, start + sz);
+                for (string& r : rest) 
+                    ans.push_back(w+(r.empty() ? "" : " ") + r);
+            }
+        }     
+        return dp[start]=ans;
+    }
+    vector<string> wordBreak(string& s, vector<string>& wordDict) {  
+        n = s.size();  
+        dp.resize(n + 1);
+        return dfs(s, wordDict, 0);
     }
 };

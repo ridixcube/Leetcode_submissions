a8c6837b0ef8e2acf3c780d734844d913c1c2879
Riddhima Langer <147099559+ridixcube@users.noreply.github.com>
2025-04-14 23:36:29 +0530

    Time: 11 ms (97.90%) | Memory: 19.7 MB (91.08%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,30 +1,34 @@
 class Solution {
-
 public:
     int maximumGain(string s, int x, int y) {
-        int points=0;;
+        int aCount = 0;
+        int bCount = 0;
+        int lesser = min(x, y);
+        int result = 0;
 
-        auto removePairs = [&](char first, char second, int point){
-            string newString;
-            int tempPoints=0;
-            for(char ch : s){
-                if(!newString.empty() && newString.back()==first && ch==second){
-                    tempPoints+=point;
-                    newString.pop_back();
-                }else{
-                    newString.push_back(ch);
+        for (char c : s) {
+            if (c > 'b') {
+                result += min(aCount, bCount) * lesser;
+                aCount = 0;
+                bCount = 0;
+            } else if (c == 'a') {
+                if (x < y && bCount > 0) {
+                    bCount--;
+                    result += y;
+                } else {
+                    aCount++;
+                }
+            } else {
+                if (x > y && aCount > 0) {
+                    aCount--;
+                    result += x;
+                } else {
+                    bCount++;
                 }
             }
-            s=newString;
-            return tempPoints;
-        };
-        if(x>y){
-            points+=removePairs('a','b',x);
-            points+=removePairs('b','a',y);
-        }else{
-            points+=removePairs('b','a',y);
-            points+=removePairs('a','b',x);
         }
-        return points;
+
+        result += min(aCount, bCount) * lesser;
+        return result;
     }
 };
